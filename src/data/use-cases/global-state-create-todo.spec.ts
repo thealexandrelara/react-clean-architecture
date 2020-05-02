602ea05faf2fe93c7691c006d6e47f34360f1847
alexandrelara-tw <alexandre.lara@thoughtworks.com>
2020-05-02 19:54:21 -0300

    test: ensure GlobalStateCreateTodo calls CreateTodoRepository with correct values

---
+++ src/data/use-cases/global-state-create-todo.spec.ts
@@ -0,0 +1,17 @@
+import { GlobalStateCreateTodo } from './global-state-create-todo'
+import { CreateTodoParams } from 'domain/use-cases/CreateTodo'
+import { CreateTodoRepository } from 'data/protocols/global-state/create-todo'
+
+describe('Global State Create Todo', () => {
+  it('should call CreateTodoRepository with correct values', () => {
+    class CreateTodoRepositoryStub implements CreateTodoRepository {
+      create(todo: CreateTodoParams) {}
+    }
+    const createTodoRepositoryStub = new CreateTodoRepositoryStub()
+    const sut = new GlobalStateCreateTodo(createTodoRepositoryStub)
+    const createSpy = jest.spyOn(createTodoRepositoryStub, 'create')
+    sut.create({ text: 'any_text' })
+
+    expect(createSpy).toHaveBeenCalledWith({ text: 'any_text' })
+  })
+})
