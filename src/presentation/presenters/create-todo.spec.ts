b003fbb806e216708eba82122d01b8d6b50fb85c
alexandrelara-tw <alexandre.lara@thoughtworks.com>
2020-05-02 18:52:50 -0300

    test: add CreateTodoParams on CreateTodoPresenter

--- src/presentation/presenters/create-todo.spec.ts
+++ src/presentation/presenters/create-todo.spec.ts
@@ -1,17 +1,20 @@
 import { CreateTodoPresenter } from './create-todo'
-import { CreateTodo } from '@/domain/use-cases/CreateTodo'
+import {
+  CreateTodo,
+  CreateTodoParams,
+} from '@/domain/use-cases/CreateTodo'
 
 describe('Create Todo Presenter', () => {
   it('should call CreateTodo with correct values', () => {
     class CreateTodoStub implements CreateTodo {
-      create(text: string) {}
+      create(todo: CreateTodoParams) {}
     }
 
     const createTodoStub = new CreateTodoStub()
     const sut = new CreateTodoPresenter(createTodoStub)
     const createSpy = jest.spyOn(createTodoStub, 'create')
-    sut.create('any_text')
+    sut.create({ text: 'any_text' })
 
-    expect(createSpy).toHaveBeenCalledWith('any_text')
+    expect(createSpy).toHaveBeenCalledWith({ text: 'any_text' })
   })
 })
