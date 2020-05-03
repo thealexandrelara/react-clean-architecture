8c9e11277dedd261643f95861ec8c17129aca28a
alexandrelara-tw <alexandre.lara@thoughtworks.com>
2020-05-02 22:35:50 -0300

    feat: add removeTodo action

--- src/infra/global-state/redux/store/ducks/todos/actions.ts
+++ src/infra/global-state/redux/store/ducks/todos/actions.ts
@@ -5,3 +5,6 @@ import { TodoTypes } from './protocols'
 
 export const createTodo = (todo: CreateTodoParams) =>
   action(TodoTypes.CREATE_TODO_REQUEST, { todo })
+
+export const removeTodo = (todo: CreateTodoParams) =>
+  action(TodoTypes.REMOVE_TODO_REQUEST, { todo })
