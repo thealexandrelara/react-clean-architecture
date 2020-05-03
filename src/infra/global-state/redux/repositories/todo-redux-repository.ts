5b97b24eeb026e5f3df1b5b9a60dc4ca5e9cfd81
alexandrelara-tw <alexandre.lara@thoughtworks.com>
2020-05-03 10:43:15 -0300

    feat: ensure TodoReduxRepository dispatches toggleTodoCompletedStatus action

--- src/infra/global-state/redux/repositories/todo-redux-repository.ts
+++ src/infra/global-state/redux/repositories/todo-redux-repository.ts
@@ -4,19 +4,22 @@ import {
   createTodo,
   removeTodo,
   editTodo,
+  toggleTodoCompletedStatus,
 } from '../store/ducks/todos/actions'
 import { CreateTodoParams } from 'domain/use-cases/CreateTodo'
+import { RemoveTodoParams } from 'domain/use-cases/RemoveTodo'
+import { EditTodoParams } from 'domain/use-cases/EditTodo'
 import { CreateTodoRepository } from 'data/protocols/global-state/create-todo'
 import { RemoveTodoRepository } from 'data/protocols/global-state/remove-todo'
 import { EditTodoRepository } from 'data/protocols/global-state/edit-todo'
-import { RemoveTodoParams } from 'domain/use-cases/RemoveTodo'
-import { EditTodoParams } from 'domain/use-cases/EditTodo'
+import { ToggleTodoCompletedStatusRepository } from 'data/protocols/global-state/toggle-todo-completed-status'
 
 export class TodoReduxRepository
   implements
     CreateTodoRepository,
     RemoveTodoRepository,
-    EditTodoRepository {
+    EditTodoRepository,
+    ToggleTodoCompletedStatusRepository {
   constructor(private readonly dispatch: Dispatch) {}
 
   create(todo: CreateTodoParams) {
@@ -29,5 +32,9 @@ export class TodoReduxRepository
 
   edit(id: string, todo: EditTodoParams) {
     this.dispatch(editTodo(id, todo))
+  }
+
+  toggle() {
+    this.dispatch(toggleTodoCompletedStatus())
   }
 }
