7117ba369458eabd6bf8a1ba0200eaa27513ee97
alexandrelara-tw <alexandre.lara@thoughtworks.com>
2020-05-03 16:01:51 -0300

    feat: add styled-components

---
+++ src/main/styles/global.ts
@@ -0,0 +1,59 @@
+import { createGlobalStyle } from 'styled-components'
+import './app.css'
+
+const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600&display=swap');
+
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+  html, body, #root {
+    height: 100%;
+    background-color: #fcfcfc;
+  }
+  body {
+    text-rendering: optimizeLegibility !important;
+    -webkit-font-smoothing: antialiased !important;
+    font-family: 'Open Sans', sans-serif;
+    font-size: 20px;
+    line-height: 26px;
+    small {
+      font-size: 18px;
+    }
+  }
+  h1 {
+    font-weight: 600;
+    font-size: 42px;
+    line-height: 50px;
+  }
+  h2 {
+    font-weight: 600;
+    font-size: 36px;
+    line-height: 40px;
+  }
+  h3 {
+    font-weight: 600;
+    font-size: 30px;
+    line-height: 36px;
+  }
+  h4 {
+    font-weight: 600;
+    font-size: 26px;
+    line-height: 32px;
+  }
+  h5 {
+    font-weight: 600;
+    font-size: 24px;
+    line-height: 30px;
+  }
+  input, button {
+  }
+  button {
+    cursor: pointer;
+  }
+`
+
+export default GlobalStyles
