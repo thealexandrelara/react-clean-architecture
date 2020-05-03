af6548e36f013bae055f4bc0039375adb783db73
alexandrelara-tw <alexandre.lara@thoughtworks.com>
2020-05-02 23:17:12 -0300

    feat: ensure TodoReduxRepository calls dispatch with correct values

--- src/infra/global-state/redux/repositories/todo-redux-repository.ts
+++ src/infra/global-state/redux/repositories/todo-redux-repository.ts
@@ -3,14 +3,20 @@ import { Dispatch } from 'redux'
 import {
   createTodo,
   removeTodo,
+  editTodo,
 } from 'infra/global-state/redux/store/ducks/todos/actions'
 import { CreateTodoParams } from 'domain/use-cases/CreateTodo'
 import { CreateTodoRepository } from 'data/protocols/global-state/create-todo'
 import { RemoveTodoRepository } from 'data/protocols/global-state/remove-todo'
+import { EditTodoRepository } from 'data/protocols/global-state/edit-todo'
 import { RemoveTodoParams } from 'domain/use-cases/RemoveTodo'
+import { EditTodoParams } from 'domain/use-cases/EditTodo'
 
 export class TodoReduxRepository
-  implements CreateTodoRepository, RemoveTodoRepository {
+  implements
+    CreateTodoRepository,
+    RemoveTodoRepository,
+    EditTodoRepository {
   constructor(private readonly dispatch: Dispatch) {}
 
   create(todo: CreateTodoParams) {
@@ -19,5 +25,9 @@ export class TodoReduxRepository
 
   remove(id: RemoveTodoParams) {
     this.dispatch(removeTodo(id))
+  }
+
+  edit(id: string, todo: EditTodoParams) {
+    this.dispatch(editTodo(id, todo))
   }
 }
