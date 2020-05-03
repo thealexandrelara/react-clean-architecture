90a319efd4a5034d7888261ffe32ac9f2ef6111c
alexandrelara-tw <alexandre.lara@thoughtworks.com>
2020-05-02 23:04:35 -0300

    refactor: rename parameter of removeTodo action

--- src/infra/global-state/redux/store/ducks/todos/actions.ts
+++ src/infra/global-state/redux/store/ducks/todos/actions.ts
@@ -1,10 +1,11 @@
 import { action } from 'typesafe-actions'
 
 import { CreateTodoParams } from 'domain/use-cases/CreateTodo'
+import { RemoveTodoParams } from 'domain/use-cases/RemoveTodo'
 import { TodoTypes } from './protocols'
 
 export const createTodo = (todo: CreateTodoParams) =>
   action(TodoTypes.CREATE_TODO_REQUEST, { todo })
 
-export const removeTodo = (todo: CreateTodoParams) =>
-  action(TodoTypes.REMOVE_TODO_REQUEST, { todo })
+export const removeTodo = (id: RemoveTodoParams) =>
+  action(TodoTypes.REMOVE_TODO_REQUEST, { id })
