0833a95051cc36bec2d6d1ccef1a53852dc86b1e
alexandrelara-tw <alexandre.lara@thoughtworks.com>
2020-05-02 22:36:51 -0300

    test: ensure TodoReduxRepository calls dispatch with correct remove action

--- src/infra/global-state/redux/repositories/todo-redux-repository.spec.ts
+++ src/infra/global-state/redux/repositories/todo-redux-repository.spec.ts
@@ -1,14 +1,29 @@
 import { TodoReduxRepository } from './todo-redux-repository'
-import { createTodo } from 'infra/global-state/redux/store/ducks/todos/actions'
+import {
+  createTodo,
+  removeTodo,
+} from 'infra/global-state/redux/store/ducks/todos/actions'
 
 describe('Todo Redux Repository', () => {
-  it('should call dispatch with correct action', () => {
-    const dispatchSpy = jest.fn()
-    const sut = new TodoReduxRepository(dispatchSpy)
-    sut.create({ text: 'any_text' })
+  describe('> create', () => {
+    it('should call dispatch with correct action', () => {
+      const dispatchSpy = jest.fn()
+      const sut = new TodoReduxRepository(dispatchSpy)
+      sut.create({ text: 'any_text', isCompleted: false })
 
-    expect(dispatchSpy).toHaveBeenCalledWith(
-      createTodo({ text: 'any_text' }),
-    )
+      expect(dispatchSpy).toHaveBeenCalledWith(
+        createTodo({ text: 'any_text', isCompleted: false }),
+      )
+    })
+  })
+
+  describe('> remove', () => {
+    it('should call dispatch with correct action', () => {
+      const dispatchSpy = jest.fn()
+      const sut = new TodoReduxRepository(dispatchSpy)
+      sut.remove('any_id')
+
+      expect(dispatchSpy).toHaveBeenCalledWith(removeTodo('any_id'))
+    })
   })
 })
