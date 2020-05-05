d114d76e9866eb61d13334606364379e5e0ae01b
alexandrelara-tw <alexandre.lara@thoughtworks.com>
2020-05-05 20:02:37 -0300

    test: ensure user can create a todo on Home page

--- src/main/pages/home/home.test.tsx
+++ src/main/pages/home/home.test.tsx
@@ -1,14 +1,24 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import {
+  renderWithAllProviders,
+  fireEvent,
+} from 'utils/tests/react-testing-library'
 
 import { Home } from './home'
 import { useHomePage, UseHomePageState } from './useHomePage'
 
-jest.mock('./useHomePage', () => ({
-  useHomePage: jest.fn(() => ({
-    todos: [],
-  })),
-}))
+type CreateTodoContext = {
+  input: HTMLElement
+  submitButton: HTMLElement
+  todoText: string
+}
+
+const createTodo = (context: CreateTodoContext) => {
+  const { input, submitButton, todoText } = context
+
+  fireEvent.change(input, { target: { value: todoText } })
+  fireEvent.click(submitButton)
+}
 
 describe('Home component', () => {
   beforeEach(() => {
@@ -16,7 +26,10 @@ describe('Home component', () => {
   })
 
   it('should contain an input', () => {
-    const { getByPlaceholderText } = render(<Home />)
+    const { getByPlaceholderText } = renderWithAllProviders(
+      <Home />,
+      null,
+    )
 
     expect(
       getByPlaceholderText('Please, type your todo text here...'),
@@ -24,17 +37,80 @@ describe('Home component', () => {
   })
 
   it('should contain an Add Todo button', () => {
-    const { getByRole } = render(<Home />)
+    const { getByRole } = renderWithAllProviders(<Home />, null)
 
     expect(getByRole('button')).toBeInTheDocument()
   })
 
-  it('should correctly display number of todos/tasks', async () => {
-    useHomePage.mockImplementationOnce(() => ({
-      todos: [1, 2, 3],
-    }))
-    const { getByText } = render(<Home />)
+  it('should not create todo with empty string', async () => {
+    const {
+      getByText,
+      getByPlaceholderText,
+      getByRole,
+    } = renderWithAllProviders(<Home />, null)
 
-    expect(getByText('3 tasks')).toBeInTheDocument()
+    expect(getByText('0 tasks')).toBeInTheDocument()
+
+    const input = getByPlaceholderText(
+      'Please, type your todo text here...',
+    )
+    const submitButton = getByRole('button')
+    createTodo({ input, submitButton, todoText: '' })
+
+    expect(getByText('0 tasks')).toBeInTheDocument()
+  })
+
+  it('should correctly display number of todos/tasks when a new todo is added', async () => {
+    const {
+      getByText,
+      getByPlaceholderText,
+      getByRole,
+    } = renderWithAllProviders(<Home />, null)
+
+    expect(getByText('0 tasks')).toBeInTheDocument()
+
+    const input = getByPlaceholderText(
+      'Please, type your todo text here...',
+    )
+    const submitButton = getByRole('button')
+    createTodo({ input, submitButton, todoText: 'any_text' })
+
+    expect(getByText('1 tasks')).toBeInTheDocument()
+  })
+
+  it('should reset input value to empty string after adding a new todo', async () => {
+    const {
+      getByText,
+      getByPlaceholderText,
+      getByRole,
+    } = renderWithAllProviders(<Home />, null)
+
+    const input = getByPlaceholderText(
+      'Please, type your todo text here...',
+    ) as HTMLInputElement
+    const submitButton = getByRole('button')
+    createTodo({ input, submitButton, todoText: 'any_text' })
+
+    expect(input.value).toEqual('')
+  })
+
+  it('should display all created todos', async () => {
+    const {
+      getByText,
+      getByPlaceholderText,
+      getByRole,
+    } = renderWithAllProviders(<Home />, null)
+
+    const input = getByPlaceholderText(
+      'Please, type your todo text here...',
+    ) as HTMLInputElement
+    const submitButton = getByRole('button')
+    createTodo({ input, submitButton, todoText: 'any_todo1' })
+    createTodo({ input, submitButton, todoText: 'any_todo2' })
+    createTodo({ input, submitButton, todoText: 'any_todo3' })
+
+    expect(getByText('any_todo1')).toBeInTheDocument()
+    expect(getByText('any_todo2')).toBeInTheDocument()
+    expect(getByText('any_todo3')).toBeInTheDocument()
   })
 })
