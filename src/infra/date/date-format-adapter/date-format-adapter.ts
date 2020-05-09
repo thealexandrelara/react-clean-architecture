28554ae27107178628d8c30b1ac2ff6a499169a9
alexandrelara-tw <alexandre.lara@thoughtworks.com>
2020-05-09 12:11:15 -0300

    feat: ensure DateFormatAdapter calls date-fns format with correct values

--- src/infra/date/date-format-adapter/date-format-adapter.ts
+++ src/infra/date/date-format-adapter/date-format-adapter.ts
@@ -3,7 +3,7 @@ import format from 'date-fns/format'
 import { DateFormatter } from 'presentation/protocols/date-formatter'
 
 export class DateFormatAdapter implements DateFormatter {
-  format(): string {
-    return 'any_format'
+  format(date: Date, expectedFormat: string): string {
+    return format(date, expectedFormat)
   }
 }
