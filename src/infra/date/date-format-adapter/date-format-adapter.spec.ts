5a2744d3718092b8b0b6b18822bc40b409b14a74
alexandrelara-tw <alexandre.lara@thoughtworks.com>
2020-05-09 12:11:19 -0300

    test: ensure DateFormatAdapter calls date-fns format with correct values

--- src/infra/date/date-format-adapter/date-format-adapter.spec.ts
+++ src/infra/date/date-format-adapter/date-format-adapter.spec.ts
@@ -1,8 +1,24 @@
+import * as format from 'date-fns/format'
 import { DateFormatAdapter } from './date-format-adapter'
+
+jest.mock('date-fns/format', () => ({
+  __esModule: true,
+  default: jest.fn().mockReturnValue('any_formatted_date'),
+}))
 
 describe('Date Format Adapter', () => {
   it('should return a string when called', () => {
     const dateFormatAdapter = new DateFormatAdapter()
-    expect(dateFormatAdapter.format()).toBeString()
+    expect(
+      dateFormatAdapter.format(new Date(), 'any_format'),
+    ).toBeString()
+  })
+
+  it('should call format with correct values', () => {
+    const dateFormatAdapter = new DateFormatAdapter()
+    const formatSpy = jest.spyOn(format, 'default')
+    const date = new Date()
+    dateFormatAdapter.format(date, 'dd-MM-yyyy')
+    expect(formatSpy).toHaveBeenCalledWith(date, 'dd-MM-yyyy')
   })
 })
